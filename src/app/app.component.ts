e6f24b200bb6ad22a54fe87fd1794d0493aaac1d
Amit Kulkarni <amit.kulkarni@ericsson.com>
2018-06-29 12:59:50 -0700

    first commit of ui code. Header and footer done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,29 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'project-bigmac';
+  searchTerm = '';
+  isCollapsed = true;
+
+  get token() {
+    return localStorage.getItem('token');
+  }
+
+  collapse() {
+    this.isCollapsed = true;
+  }
+
+  closeDropdown(dropdown) {
+    dropdown.close();
+  }
+
+  logout() {
+    console.log('Inside logout');
+  }
+
+  search() {
+    console.log('Inside search');
+  }
 }
